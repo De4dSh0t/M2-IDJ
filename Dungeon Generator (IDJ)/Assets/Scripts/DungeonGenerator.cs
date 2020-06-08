1521eefdceb959905343b65ab976b8181cc6dd99
De4dSh0t <ruipsilva2001@hotmail.com>
2020-06-08 17:31:21 +0100

    Added "GeneratePath()"

--- Dungeon Generator (IDJ)/Assets/Scripts/DungeonGenerator.cs
+++ Dungeon Generator (IDJ)/Assets/Scripts/DungeonGenerator.cs
@@ -26,11 +26,72 @@ public class DungeonGenerator : MonoBehaviour
     /// </summary>
     private void GenerateRoom(int x, int y, int xSize, int ySize, int nEntrances)
     {
-        RoomGenerator.ground = ground;
-        RoomGenerator.topWall = topWall;
-        RoomGenerator.bottomWall = bottomWall;
-        RoomGenerator.groundMap = groundMap;
-        RoomGenerator.wallMap = wallMap;
-        RoomGenerator.RectangularRoom(x, y, xSize, ySize, nEntrances);
+        Room room = new Room
+        {
+            ground = ground,
+            topWall = topWall,
+            bottomWall = bottomWall,
+            groundMap = groundMap,
+            wallMap = wallMap
+        };
+        room.RectangularRoom(x, y, xSize, ySize, nEntrances);
+        GeneratePath(room, room.entranceWidth, 20);
+    }
+
+    private void GeneratePath(Room room, int entranceWidth, int pathLength)
+    {
+        int xEntrance = 0;
+        int yEntrance = 0;
+        int direction = 0;
+        foreach (var kvp in room.entrancePosition)
+        {
+            direction = kvp.Key;
+            xEntrance = kvp.Value[0];
+            yEntrance = kvp.Value[1];
+            if (direction == 1) //Left
+            {
+                for (int tileX = xEntrance; tileX > xEntrance - pathLength; tileX--)
+                {
+                    for (int tileY = yEntrance; tileY < yEntrance + entranceWidth; tileY++)
+                    {
+                        Vector3Int pos = new Vector3Int(tileX, tileY, 0);
+                        groundMap.SetTile(pos, ground);
+                    }
+                }
+            }
+            if (direction == 2) //Right
+            {
+                for (int tileX = xEntrance; tileX < xEntrance + pathLength; tileX++)
+                {
+                    for (int tileY = yEntrance; tileY < yEntrance + entranceWidth; tileY++)
+                    {
+                        Vector3Int pos = new Vector3Int(tileX, tileY, 0);
+                        groundMap.SetTile(pos, ground);
+                    }
+                }
+            }
+            if (direction == 3) //Up
+            {
+                for (int tileX = xEntrance; tileX < xEntrance + entranceWidth; tileX++)
+                {
+                    for (int tileY = yEntrance; tileY < yEntrance + pathLength; tileY++)
+                    {
+                        Vector3Int pos = new Vector3Int(tileX, tileY, 0);
+                        groundMap.SetTile(pos, ground);
+                    }
+                }
+            }
+            if (direction == 4) //Down
+            {
+                for (int tileX = xEntrance; tileX < xEntrance + entranceWidth; tileX++)
+                {
+                    for (int tileY = yEntrance; tileY > yEntrance - pathLength; tileY--)
+                    {
+                        Vector3Int pos = new Vector3Int(tileX, tileY, 0);
+                        groundMap.SetTile(pos, ground);
+                    }
+                }
+            }
+        }
     }
 }
