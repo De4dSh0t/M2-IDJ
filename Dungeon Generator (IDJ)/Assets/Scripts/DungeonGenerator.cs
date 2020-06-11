06b03a87b5013d97d6a0f7a5c7a22c8fd99b08a6
De4dSh0t <ruipsilva2001@hotmail.com>
2020-06-11 18:13:16 +0100

    Updated "DungeonGenerator" logic

--- Dungeon Generator (IDJ)/Assets/Scripts/DungeonGenerator.cs
+++ Dungeon Generator (IDJ)/Assets/Scripts/DungeonGenerator.cs
@@ -15,7 +15,8 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField] private int maxRoomXSize;
     [SerializeField] private int maxRoomYSize;
     [SerializeField] private int entranceWidth;
-    [SerializeField] private RoomInfo[] roomInfo;
+    [SerializeField] private int pathLength;
+    [SerializeField] private int corridorDeviationRate; //Controls how much the corridor deviates
     private int removePossibleEntrance; //This will prevent the entrances from being repeated on the same side.
     private int minRoomXSize;
     private int minRoomYSize;
@@ -47,7 +48,24 @@ public class DungeonGenerator : MonoBehaviour
         room.entranceWidth = entranceWidth;
         room.possibleDirections.Remove(removePossibleEntrance); 
         room.RectangularRoom(x, y, xSize, ySize, nEntrances);
-        GeneratePath(room, room.entranceWidth, 20);
+        GeneratePath(room, room.entranceWidth, pathLength);
+    }
+
+    /// <summary>
+    /// Generates a square to be used when generating path/corridor
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    private void CorridorSquare(int x, int y)
+    {
+        for (int tileX = x; tileX < x + entranceWidth; tileX++)
+        {
+            for (int tileY = y; tileY < y + entranceWidth; tileY++)
+            {
+                Vector3Int pos = new Vector3Int(tileX, tileY, 0);
+                groundMap.SetTile(pos, ground);
+            }
+        }
     }
 
     /// <summary>
@@ -63,92 +81,95 @@ public class DungeonGenerator : MonoBehaviour
         int direction = 0;
         int xSize = 0;
         int ySize = 0;
+        List<int> possibleTurnDirection = new List<int>();
+        int iPathLength = pathLength; //Controls how many iterations can be done
         
         foreach (var entrances in room.entrancePosition)
         {
-            direction = entrances.Key;
+            direction = entrances.Key; //Entrance Direction
             xEntrance = entrances.Value[0];
             yEntrance = entrances.Value[1];
-            int nEntrances = Random.Range(0, 4);
+            int nEntrances = Random.Range(1, 4);
+            int tileX = xEntrance;
+            int tileY = yEntrance;
+            bool first = true; //Used when direction is 2 (right) and 3 (up), because the first tile should be a block ahead
             xSize = Random.Range(minRoomXSize, maxRoomXSize);
             ySize = Random.Range(minRoomYSize, maxRoomYSize);
-            
-            if (direction == 1) //Left
-            {
-                for (int tileX = xEntrance; tileX > xEntrance - pathLength; tileX--)
-                {
-                    for (int tileY = yEntrance; tileY < yEntrance + entranceWidth; tileY++)
-                    {
-                        Vector3Int pos = new Vector3Int(tileX, tileY, 0);
-                        groundMap.SetTile(pos, ground);
-                    }
-                }
-                maxNumPaths -= nEntrances;
-                if (maxNumPaths > 0)
-                {
-                    removePossibleEntrance = 2; //Because corridor comes from the right side of the next room
-                    GenerateRoom(xEntrance - pathLength, yEntrance - 1, xSize, ySize, nEntrances);
-                }
-            }
-
-            if (direction == 2) //Right
-            {
-                for (int tileX = xEntrance; tileX < xEntrance + pathLength; tileX++)
-                {
-                    for (int tileY = yEntrance; tileY < yEntrance + entranceWidth; tileY++)
-                    {
-                        Vector3Int pos = new Vector3Int(tileX, tileY, 0);
-                        groundMap.SetTile(pos, ground);
-                    }
-                }
-                maxNumPaths -= nEntrances;
-                if (maxNumPaths > 0)
-                {
-                    removePossibleEntrance = 1; //Because corridor comes from the left side of the next room
-                    GenerateRoom(xEntrance + pathLength, yEntrance - 1, xSize, ySize, nEntrances);
-                }
-            }
-
-            if (direction == 3) //Up
-            {
-                for (int tileX = xEntrance; tileX < xEntrance + entranceWidth; tileX++)
-                {
-                    for (int tileY = yEntrance; tileY < yEntrance + pathLength; tileY++)
-                    {
-                        Vector3Int pos = new Vector3Int(tileX, tileY, 0);
-                        groundMap.SetTile(pos, ground);
-                    }
-                }
-                maxNumPaths -= nEntrances;
-                if (maxNumPaths > 0)
-                {
-                    removePossibleEntrance = 4; //Because corridor comes from the bottom of the next room
-                    GenerateRoom(xEntrance - 1, yEntrance + pathLength, xSize, ySize, nEntrances);
-                }
-            }
-
-            if (direction == 4) //Down
-            {
-                for (int tileX = xEntrance; tileX < xEntrance + entranceWidth; tileX++)
-                {
-                    for (int tileY = yEntrance; tileY > yEntrance - pathLength; tileY--)
-                    {
-                        Vector3Int pos = new Vector3Int(tileX, tileY, 0);
-                        groundMap.SetTile(pos, ground);
-                    }
-                }
-                maxNumPaths -= nEntrances;
-                if (maxNumPaths > 0)
-                {
-                    removePossibleEntrance = 3; //Because corridor comes from the top of the next room
-                    GenerateRoom(xEntrance - 1, yEntrance - pathLength, xSize, ySize, nEntrances);
-                }
-            }
             
             if (maxNumPaths <= 0)
             {
                 nEntrances = 0;
             }
+
+            for (int i = 0; i < pathLength; i++)
+            {
+                switch (direction)
+                {
+                    case 1: //Left
+                    {
+                        possibleTurnDirection = new List<int>() {1, 3, 4};
+
+                        tileX -= entranceWidth;
+                        CorridorSquare(tileX, tileY);
+                        
+                        break;
+                    }
+                    case 2: //Right
+                    {
+                        possibleTurnDirection = new List<int>() {2, 3, 4};
+
+                        if (first)
+                        {
+                            tileX += 1;
+                            CorridorSquare(tileX, tileY);
+                            first = false;
+                        }
+                        else
+                        {
+                            tileX += entranceWidth;
+                            CorridorSquare(tileX, tileY);
+                        }
+
+                        break;
+                    }
+                    case 3: //Up
+                    {
+                        possibleTurnDirection = new List<int>() {1, 2, 3};
+
+                        if (first)
+                        {
+                            tileY += 1;
+                            CorridorSquare(tileX, tileY);
+                            first = false;
+                        }
+                        else
+                        {
+                            tileY += entranceWidth;
+                            CorridorSquare(tileX, tileY);
+                        }
+                        
+                        break;
+                    }
+                    case 4: //Down
+                    {
+                        possibleTurnDirection = new List<int>() {1, 2, 4};
+
+                        tileY -= entranceWidth;
+                        CorridorSquare(tileX, tileY);
+                        
+                        break;
+                    }
+                }
+                
+                direction = possibleTurnDirection[Random.Range(0, possibleTurnDirection.Count)];
+
+                if (i == pathLength - 1 && maxNumPaths > 0) //Last iteration
+                {
+                    GenerateRoom(tileX - xSize, tileY - ySize, xSize, ySize, nEntrances);
+                }
+            }
+
+            maxNumPaths--;
         }
     }
 }
